7a811e8ce4aef6e112f26cc48b17f08085cc6fce
mr7-seven <94348725+mr7-seven@users.noreply.github.com>
2026-06-11 09:59:16 +0800

    Modal plot
    Modal plot

---
+++ Sesi 14 - Datatable + Modal BS/modal-plot.php
@@ -0,0 +1,36 @@
+<div class="modal fade"
+    id="plotModal"
+    tabindex="-1">
+
+    <div class="modal-dialog modal-xl">
+
+        <div class="modal-content">
+
+            <div class="modal-header">
+
+                <h5 class="modal-title">
+                    Grafik {{ selectedParameter }}
+                </h5>
+
+                <button
+                    type="button"
+                    class="btn-close"
+                    data-bs-dismiss="modal">
+                </button>
+
+            </div>
+
+            <div class="modal-body">
+
+                <div
+                    id="plotContainer"
+                    style="height:500px;">
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</div>
